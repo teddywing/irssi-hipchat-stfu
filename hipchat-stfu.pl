4c57303e4653bc332f5506440d40fc4df33b2420
Teddy Wing <git@teddywing.com>
2015-05-31 17:58:58 -0400

    Add base irssi script
    New file containing the minimum defaults needed for an irssi script,
    copied from http://juerd.nl/site.plp/irssiscripttut.
    
    Sets up `$VERSION` & `%IRSSI` variables.

---
+++ hipchat-stfu.pl
@@ -0,0 +1,12 @@
+use strict;
+use vars qw($VERSION %IRSSI);
+
+use Irssi;
+$VERSION = '1.00';
+%IRSSI = (
+	authors     => 'Teddy Wing',
+	contact     => 'irssi@teddywing.com',
+	name        => 'HipChat STFU',
+	description => 'Silences annoying messages originating from HipChat.',
+	license     => 'MIT',
+);
