2706cdbcc579220d5ee352410640b2c800f3e733
Teddy Wing <git@teddywing.com>
2015-06-13 23:59:29 -0400

    test.pl: Add some real test cases
    * Use `Test::More` instead of `Test::Simple` so I can take advantage of
      the `is` test function
    * Get rid of my dummy test tests
    * Add some tests based on HipChat output for what I'd like that output
      to be prettified to

--- test.pl
+++ test.pl
@@ -2,7 +2,30 @@
 
 use strict;
 
-use Test::Simple tests => 2;
+use Test::More tests => 9;
 
-ok( 1 + 1 == 2 );
-ok( 2 + 2 == 5 );
+my $title_message = <<'END_MESSAGE';
+SC-1000 : Some kind of issue text that is kind of long and describes the problem that we saw during testing.
+https://somecompany.atlassian.net/browse/SC-2169
+Created by Person Name
+END_MESSAGE
+
+is(prettify_hipchat('  <img src="https://somecompany.atlassian.net/images/16jira.png" height="16" width="16" />   <a href="https://somecompany.atlassian.net/browse/SC-2169"><b>SC-1000 : Some kind of issue text that is kind of long and describes the problem that we saw during testing.</b></a> Created by <a href="https://somecompany.atlassian.net/secure/ViewProfile.jspa?name=PersonN">Person Name</a><br />'),
+	$title_message,
+	'Title, link, and creator');
+is(prettify_hipchat('<span style="color: dimgrey;">Type:</span>  <img src="https://somecompany.atlassian.net/images/icons/issuetypes/bug.png" height="16" width="16" /> Bug  '),
+	'Type: Bug',
+	'Type');
+is(prettify_hipchat('    <span style="color: dimgrey;">Priority:</span>      <img src="http://pixshark.com/white-box-icon.htm" height="16" width="16" />'),
+	'Priority:',
+	'Priority label');
+is(prettify_hipchat(' Not prioritized  '), 'Not prioritized', 'Priority tag body');
+is(prettify_hipchat('<span style="color: dimgrey;">Status:</span>          '),
+	'',
+	'Status label');
+is(prettify_hipchat('    <span class="aui-lozenge aui-lozenge-complete">'),
+	'',
+	'<span ...>');
+is(prettify_hipchat('        To Do'), 'To Do', 'To Do');
+is(prettify_hipchat('    </span>'), '', '</span>');
+is(prettify_hipchat('<br />'), '', '<br />');
